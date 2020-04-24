d5cd32d8ba0486d257613176a6f37667a9b4ef16
Rachael Ewins <rachaelewins@hotmail.com>
2020-04-24 16:28:37 +0000

    3.16 Done

--- session3/challenge/16_hash.rb
+++ session3/challenge/16_hash.rb
@@ -15,3 +15,11 @@
 # head = {:data => 6, :next => head}
 # print_list head                    # >> "6\n5\n4\n3\n2\n1\n"
 
+def print_list(hsh,out="")
+    out << hsh[:data].to_s + "\n"
+    if hsh[:next] == nil
+        puts out
+        return
+    end
+    print_list(hsh[:next],out)
+end
